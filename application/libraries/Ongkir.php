19dbf579bab8ed325e2e3a59aebdc5298a033147
Panji Iman Baskoro <panjidia995@gmail.com>
2021-01-19 23:01:15 +0700

    first

--- application/libraries/Ongkir.php
+++ application/libraries/Ongkir.php
@@ -3,7 +3,33 @@ class Ongkir {
 
    function test()
   {
-    return 'Hello, ItSolutionStuff.com';
+    $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://ruangapi.com/api/v1/provinces",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "accept: application/json",
+            "authorization: tgdE2vHA696lTUUSyA3OA90HYNuVpuoRTerQkOoR",
+            "content-type: application/json"
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+        return $response;
+        }
   } 
 }
 ?>
