7644a25a836d479789a362e40fb58b4b7ad0a3fe
Panji Iman Baskoro <panjidia995@gmail.com>
2021-01-20 13:55:28 +0700

    test endpoint

--- application/libraries/Ongkir.php
+++ application/libraries/Ongkir.php
@@ -40,12 +40,37 @@ class Ongkir {
     return $end.$param;
   }
 
-  private function mainQuery($param){
+  private function mainQuery($param,$extraParam=null){
+    $curl = curl_init();
 
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "$param",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+        "accept: application/json",
+        "authorization: $this->apikey",
+        "content-type: application/json"
+    ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+    $data = json_decode($response, true);
+    return $data['data']['results'];
   }
 
   public function getAllProvinces(){
-    echo $this->getEndpoint("provinces");
+    $param = $this->getEndpoint("provinces");
+    $result = $this->mainQuery($param);
+    
+    return $result;
   }
 }
 ?>
